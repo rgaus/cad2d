1b5ddb84f43dab792dfd98028d625152afb90295
Ryan Gaus <rsg1egoman@gmail.com>
2026-05-19 19:07:41 -0400

    feat: add secondary hover tooltip styling

--- src/app/components/ConstraintLengthInput.tsx
+++ src/app/components/ConstraintLengthInput.tsx
@@ -216,8 +216,8 @@ export default forwardRef<ConstraintLengthInputHandle, ConstraintLengthInputProp
       ) : null}
 
       {inputFocused && onTabPress ? (
-        <div className="absolute -bottom-7 z-30">
-          <HoverTooltip>
+        <div className="absolute -bottom-6 -left-0.5 z-30">
+          <HoverTooltip variant="secondary">
             <div className="flex items-center gap-2">
               <KeyboardShortcut label="Next">tab</KeyboardShortcut>
             </div>
