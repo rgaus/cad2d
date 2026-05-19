c6336fa63cb920c3a9123e42398449a76eeef950
Ryan Gaus <rsg1egoman@gmail.com>
2026-05-19 18:51:01 -0400

    fix: make constraint box respect default sheet unit

--- src/components/ConstraintsRenderer.tsx
+++ src/components/ConstraintsRenderer.tsx
@@ -191,7 +191,7 @@ const ConstraintTooltips: React.FunctionComponent = () => {
                     let nextIndex = (index + 1) % workingConstraints.length;
                     constraintLengthInputsRef.current.get(nextIndex)?.focus();
                   }}
-                  defaultUnit="cm"
+                  defaultUnit={sheet.defaultUnit}
                 />
               </div>
             );
