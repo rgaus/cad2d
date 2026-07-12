e2a5bbe9aff7817c294e21e0fd77d26d23606aa3
Ryan Gaus <rsg1egoman@gmail.com>
2026-07-12 14:56:46 -0400

    fix: run npm run format

--- src/lib/geometry/constraints/index.ts
+++ src/lib/geometry/constraints/index.ts
@@ -122,7 +122,4 @@ export { VerticalConstraint, type VerticalConstraintTemplate } from './vertical'
 
 export { ColinearConstraint, type ColinearConstraintTemplate } from './colinear';
 
-export {
-  ConstrainedTrack,
-  type ConstrainedTrackPath,
-} from './compute-constrained-tracks';
+export { ConstrainedTrack, type ConstrainedTrackPath } from './compute-constrained-tracks';
