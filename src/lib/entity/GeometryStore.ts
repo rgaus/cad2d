2ab2e7c06e441349835cf86ac639f110c6b7b063
Ryan Gaus <rsg1egoman@gmail.com>
2026-09-18 08:23:11 -0400

    feat: add special case to rerun syncFillColor on geometry when new filters get added to it

--- src/lib/entity/GeometryStore.ts
+++ src/lib/entity/GeometryStore.ts
@@ -493,7 +493,59 @@ export class GeometryStore extends EventEmitter<GeometryStoreEvents> {
    * Does NOT record to history. Used by HistoryManager redo.
    */
   addDirect(entity: Entity): void {
+    console.log('ADD DIRECT', entity);
+
+    // // Special case: before inserting a geometry, check to see if it's a non closed polygon which is
+    // // being mirrored over a line / etc via a filter, and if it is, then add the required
+    // // FillColorComponent.
+    // if (Entity.hasComponent(entity, GeometryComponent)) {
+    //   const filters = this.listWithComponent(FilterComponent);
+    //   if (filters.length > 0) {
+    //     const geometryIds = new Set(
+    //       filters
+    //         .map((f) => FilterComponent.get(f).geometryId)
+    //         .filter((id) => entity.id === id)
+    //     );
+
+    //     let acc = entity;
+    //     for (const geometryId of geometryIds) {
+    //       const geometry = this.getByIdWithComponent(geometryId, GeometryComponent);
+    //       console.log('geom:', geometryId, geometry);
+    //       if (!geometry) {
+    //         continue;
+    //       }
+    //       const [output] = FilterComponent.syncFillColor(
+    //         acc,
+    //         this.findFiltersByGeometryId(geometryId),
+    //       );
+    //       if (output !== entity) {
+    //         acc = output;
+    //       }
+    //     }
+    //     entity = acc;
+    //   }
+    // }
+
+    // Special case: before inserting a geometry, check to see if it's a non closed polygon which is
+    // being mirrored over a line / etc via a filter, and if it is, then add the required
+    // FillColorComponent.
+    if (Entity.hasComponent(entity, FilterComponent)) {
+      const geometryId = FilterComponent.get(entity).geometryId;
+      const geometry = this.getByIdWithComponent(geometryId, GeometryComponent);
+      console.log('geom:', geometryId, geometry);
+      if (geometry) {
+        const [output] = FilterComponent.syncFillColor(
+          geometry,
+          this.findFiltersByGeometryId(geometryId),
+        );
+        if (output !== geometry) {
+          this.updateByIdDirect(geometryId, output);
+        }
+      }
+    }
+
     this.geometryById.set(entity.id, entity);
+
     if (
       Entity.hasComponent(entity, GeometryComponent) ||
       Entity.hasComponent(entity, DatumComponent)
